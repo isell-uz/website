0305495a37b7acab3599b879e3252835b6c9d10f
Mukhammad Ilkhomov <61352680+admin1475963@users.noreply.github.com>
2025-02-12 17:53:32 +0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"], // This will watch your src directory
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
